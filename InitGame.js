8fe22b3d5e344ca03369c61d1a1ec6ff0da30391
Mayank Rastogi <mayankrustey2002@gmail.com>
2022-04-06 13:39:16 -0700

    Server removed

--- InitGame.js
+++ InitGame.js
@@ -5,15 +5,15 @@
 
 (function(){
     //URL to establish conncetion to
-    const url = 'ws://10.33.34.32:8085'
-    connection = new WebSocket(url)
-    connection2 = new WebSocket(url)
-    connection3 = new WebSocket(url)
-    connection4 = new WebSocket(url)
+    // const url = 'ws://10.33.34.32:8085'
+    // connection = new WebSocket(url)
+    // connection2 = new WebSocket(url)
+    // connection3 = new WebSocket(url)
+    // connection4 = new WebSocket(url)
 
-    connection.onerror = error => {
-        console.error(error)
-    }
+    // connection.onerror = error => {
+    //     console.error(error)
+    // }
     // Defining world container that holds the game
     const world = new pyworld({
         element: document.querySelector(".rpg-container")
